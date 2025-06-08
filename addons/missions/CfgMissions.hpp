579beaee69b1919523e845ce8f64cb0b4ab52ddc
Hypoxic <martini_a@outlook.com>
2025-06-08 14:08:16 -0600

    Update CfgMissions.hpp

--- addons/missions/CfgMissions.hpp
+++ addons/missions/CfgMissions.hpp
@@ -1,8 +1,13 @@
 class CfgMissions {
     class MPMissions {
+        class CarrierStrike_Malden_ExtraSmall
+        {
+            briefingName = "Carrier Strike - Malden";
+            directory = "z\carrierstrike\addons\missions\mpmissions\CarrierStrike_Malden_ExtraSmall.Malden";
+        };
         class CarrierStrike_Malden_Small
         {
-            briefingName = "Carrier Strike - Malden (S)";
+            briefingName = "Carrier Strike - Malden";
             directory = "z\carrierstrike\addons\missions\mpmissions\CarrierStrike_Malden_Small.Malden";
         };
     };
