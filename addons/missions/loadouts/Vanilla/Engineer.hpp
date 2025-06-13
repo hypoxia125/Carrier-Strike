95bbc033e2c4c0ed8415147169014ea7e30e1bd3
Hypoxic <martini_a@outlook.com>
2025-06-12 20:28:04 -0600

    Update Engineer.hpp

--- addons/missions/loadouts/Vanilla/Engineer.hpp
+++ addons/missions/loadouts/Vanilla/Engineer.hpp
@@ -8,7 +8,7 @@ class Tier2_West_Engineer
     backpack = "B_Kitbag_mcamo_Eng";
     weapons[] = {"arifle_MXC_Holo_pointer_F", "hgun_P07_F", "Throw", "Put"};
     magazines[] = {"30Rnd_65x39_caseless_mag", "Chemlight_green", "30Rnd_65x39_caseless_mag", "30Rnd_65x39_caseless_mag", "30Rnd_65x39_caseless_mag", "30Rnd_65x39_caseless_mag", "16Rnd_9x21_Mag", "16Rnd_9x21_Mag", "HandGrenade", "HandGrenade", "SmokeShell", "SmokeShellGreen", "SmokeShellBlue", "SmokeShellOrange", "Chemlight_green", "SatchelCharge_Remote_Mag", "DemoCharge_Remote_Mag", "DemoCharge_Remote_Mag", "30Rnd_65x39_caseless_mag", "16Rnd_9x21_Mag"};
-    items[] = {"FirstAidKit", "ACE_Clacker", "ACE_DefusalKit", "ToolKit", "MineDetector"};
+    items[] = {"FirstAidKit", "ToolKit", "MineDetector"};
     linkedItems[] = {"V_Chestrig_rgr", "H_HelmetB_desert", "ItemMap", "ItemCompass", "ItemWatch", "ItemRadio", "NVGoggles", "acc_pointer_IR", "optic_Holosight"};
 };
 
@@ -22,7 +22,7 @@ class Tier2_West_Engineer_Pacific
     backpack = "B_Kitbag_rgr_BTEng_F";
     weapons[] = {"arifle_MXC_khk_Holo_Pointer_F", "hgun_P07_khk_F", "Throw", "Put"};
     magazines[] = {"30Rnd_65x39_caseless_khaki_mag", "Chemlight_green", "30Rnd_65x39_caseless_khaki_mag", "30Rnd_65x39_caseless_khaki_mag", "30Rnd_65x39_caseless_khaki_mag", "30Rnd_65x39_caseless_khaki_mag", "16Rnd_9x21_Mag", "16Rnd_9x21_Mag", "HandGrenade", "HandGrenade", "SmokeShell", "SmokeShellGreen", "SmokeShellBlue", "SmokeShellOrange", "Chemlight_green", "SatchelCharge_Remote_Mag", "DemoCharge_Remote_Mag", "DemoCharge_Remote_Mag", "30Rnd_65x39_caseless_khaki_mag", "16Rnd_9x21_Mag"};
-    items[] = {"FirstAidKit", "ACE_Clacker", "ACE_DefusalKit", "ToolKit", "MineDetector"};
+    items[] = {"FirstAidKit", "ToolKit", "MineDetector"};
     linkedItems[] = {"V_Chestrig_rgr", "H_HelmetB_tna_F", "ItemMap", "ItemCompass", "ItemWatch", "ItemRadio", "NVGoggles_tna_F", "acc_pointer_IR", "optic_Holosight_khk_F"};
 };
 
@@ -36,7 +36,7 @@ class Tier2_West_Engineer_Woodland
     backpack = "B_Kitbag_rgr_BTEng_F";
     weapons[] = {"arifle_MXC_Black_Holo_Pointer_F", "hgun_P07_khk_F", "Throw", "Put"};
     magazines[] = {"30Rnd_65x39_caseless_black_mag", "Chemlight_green", "30Rnd_65x39_caseless_black_mag", "30Rnd_65x39_caseless_black_mag", "30Rnd_65x39_caseless_black_mag", "30Rnd_65x39_caseless_black_mag", "16Rnd_9x21_Mag", "16Rnd_9x21_Mag", "HandGrenade", "HandGrenade", "SmokeShell", "SmokeShellGreen", "SmokeShellBlue", "SmokeShellOrange", "Chemlight_green", "SatchelCharge_Remote_Mag", "DemoCharge_Remote_Mag", "DemoCharge_Remote_Mag", "30Rnd_65x39_caseless_black_mag", "16Rnd_9x21_Mag"};
-    items[] = {"FirstAidKit", "ACE_Clacker", "ACE_DefusalKit", "ToolKit", "MineDetector"};
+    items[] = {"FirstAidKit", "ToolKit", "MineDetector"};
     linkedItems[] = {"V_Chestrig_rgr", "H_HelmetB_plain_wdl", "ItemMap", "ItemCompass", "ItemWatch", "ItemRadio", "NVGoggles_INDEP", "acc_pointer_IR", "optic_Holosight_blk_F"};
 };
 
@@ -50,7 +50,7 @@ class Tier2_East_Engineer
     backpack = "B_Carryall_ocamo_Eng";
     weapons[] = {"arifle_Katiba_C_ACO_pointer_F", "hgun_Rook40_F", "Throw", "Put"};
     magazines[] = {"30Rnd_65x39_caseless_green", "Chemlight_red", "30Rnd_65x39_caseless_green", "30Rnd_65x39_caseless_green", "30Rnd_65x39_caseless_green", "30Rnd_65x39_caseless_green", "16Rnd_9x21_Mag", "16Rnd_9x21_Mag", "HandGrenade", "HandGrenade", "SmokeShell", "SmokeShellRed", "SmokeShellOrange", "SmokeShellYellow", "Chemlight_red", "SatchelCharge_Remote_Mag", "DemoCharge_Remote_Mag", "DemoCharge_Remote_Mag", "30Rnd_65x39_caseless_green", "16Rnd_9x21_Mag"};
-    items[] = {"FirstAidKit", "ACE_Clacker", "ACE_DefusalKit", "ToolKit", "MineDetector"};
+    items[] = {"FirstAidKit", "ToolKit", "MineDetector"};
     linkedItems[] = {"V_HarnessO_brn", "H_HelmetO_ocamo", "ItemMap", "ItemCompass", "ItemWatch", "ItemRadio", "NVGoggles_OPFOR", "acc_pointer_IR", "optic_ACO_grn"};
 };
 
@@ -64,7 +64,7 @@ class Tier2_East_Engineer_Urban
     backpack = "B_Carryall_oucamo_Eng";
     weapons[] = {"arifle_Katiba_C_ACO_pointer_F", "hgun_Rook40_F", "Throw", "Put"};
     magazines[] = {"30Rnd_65x39_caseless_green", "Chemlight_red", "30Rnd_65x39_caseless_green", "30Rnd_65x39_caseless_green", "30Rnd_65x39_caseless_green", "30Rnd_65x39_caseless_green", "16Rnd_9x21_Mag", "16Rnd_9x21_Mag", "HandGrenade", "HandGrenade", "SmokeShell", "SmokeShellRed", "SmokeShellOrange", "SmokeShellYellow", "Chemlight_red", "SatchelCharge_Remote_Mag", "DemoCharge_Remote_Mag", "DemoCharge_Remote_Mag", "30Rnd_65x39_caseless_green", "16Rnd_9x21_Mag"};
-    items[] = {"FirstAidKit", "ACE_Clacker", "ACE_DefusalKit", "ToolKit", "MineDetector"};
+    items[] = {"FirstAidKit", "ToolKit", "MineDetector"};
     linkedItems[] = {"V_HarnessO_gry", "H_HelmetO_oucamo", "ItemMap", "ItemCompass", "ItemWatch", "ItemRadio", "NVGoggles_OPFOR", "acc_pointer_IR", "optic_ACO_grn"};
 };
 
@@ -78,6 +78,6 @@ class Tier2_East_Engineer_Pacific
     backpack = "B_Carryall_ghex_OTEng_F";
     weapons[] = {"arifle_CTAR_blk_ACO_Pointer_F", "hgun_Rook40_F", "Throw", "Put"};
     magazines[] = {"30Rnd_580x42_Mag_F", "30Rnd_580x42_Mag_F", "SmokeShell", "30Rnd_580x42_Mag_F", "30Rnd_580x42_Mag_F", "30Rnd_580x42_Mag_F", "16Rnd_9x21_Mag", "16Rnd_9x21_Mag", "HandGrenade", "HandGrenade", "SmokeShellRed", "SmokeShellOrange", "SmokeShellYellow", "Chemlight_red", "Chemlight_red", "SatchelCharge_Remote_Mag", "DemoCharge_Remote_Mag", "DemoCharge_Remote_Mag", "30Rnd_580x42_Mag_F", "16Rnd_9x21_Mag"};
-    items[] = {"FirstAidKit", "ACE_Clacker", "ACE_DefusalKit", "ToolKit", "MineDetector"};
+    items[] = {"FirstAidKit", "ToolKit", "MineDetector"};
     linkedItems[] = {"V_HarnessO_ghex_F", "H_HelmetO_ghex_F", "ItemMap", "ItemCompass", "ItemWatch", "ItemRadio", "O_NVGoggles_ghex_F", "acc_pointer_IR", "optic_ACO_grn"};
 };
