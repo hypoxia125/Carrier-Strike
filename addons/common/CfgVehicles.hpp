79b873748584f55d0485171e724e234c473dc820
Hypoxic <martini_a@outlook.com>
2025-06-25 23:37:53 -0600

    map + texture update
    - Created textures through vehicle classes for independent vehicles that will be used by Opfor
    - Updated map descriptions to show what loadout preset is loaded

--- addons/common/CfgVehicles.hpp
+++ addons/common/CfgVehicles.hpp
@@ -13,4 +13,29 @@ class CfgVehicles {
         uniform = "";
         backpack = "";
     };
+
+    class I_APC_tracked_03_cannon_F;
+    class CarrierStrike_O_APC_tracked_03_cannon_F: I_APC_tracked_03_cannon_F {
+        displayName = "FV-720 Mora (CSAT)";
+        faction = "OPF_F";
+        hiddenSelectionsTextures[] = {
+            "\z\carrierstrike\addons\common\data\textures\I_APC_tracked_03_cannon_F\apc_tracked_03_ext_opfor_co.paa",
+            "\z\carrierstrike\addons\common\data\textures\I_APC_tracked_03_cannon_F\apc_tracked_03_ext2_opfor_co.paa",
+            "\z\carrierstrike\addons\common\data\textures\camonet_CSAT_CO.paa",
+            "\z\carrierstrike\addons\common\data\textures\cage_csat_co.paa"
+        };
+        side = 0;
+        typicalCargo[] = {"O_Soldier_F"};
+    };
+
+    class B_Heli_Light_01_F;
+    class CarrierStrike_O_Heli_Light_01_F: B_Heli_Light_01_F {
+        displayName = "MH-9 Hummingbird (CSAT)";
+        faction = "OPF_F";
+        hiddenSelectionsTextures[] = {
+            "\z\carrierstrike\addons\common\data\textures\B_Heli_Light_01_F\Heli_Light_01_ext_Opfor_CO.paa"
+        };
+        side = 0;
+        typicalCargo[] = {"O_HeliPilot_F"};
+    };
 };
